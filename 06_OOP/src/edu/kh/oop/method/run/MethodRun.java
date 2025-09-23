553a96dde20113c636379238c0a3abe2a4d6705e
juyoung-97 <boduni20@gmail.com>
2025-09-23 16:33:53 +0900

    9 / 23 OOP +

---
+++ 06_OOP/src/edu/kh/oop/method/run/MethodRun.java
@@ -0,0 +1,12 @@
+package edu.kh.oop.method.run;
+
+import edu.kh.oop.method.model.service.MethodExample;
+
+public class MethodRun {
+	
+	public static void main(String[] args) {
+		MethodExample ex = new MethodExample();
+		// ex.method1();
+	}
+
+}
