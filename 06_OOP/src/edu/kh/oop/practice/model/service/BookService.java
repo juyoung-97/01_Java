5b14e29490c89769ff7ee3778cfaefe05df342b7
juyoung-97 <boduni20@gmail.com>
2025-09-25 08:52:48 +0900

    hero OOP 공부 더해야함 다시작성요망

--- 06_OOP/src/edu/kh/oop/practice/model/service/BookService.java
+++ 06_OOP/src/edu/kh/oop/practice/model/service/BookService.java
@@ -1,42 +1,41 @@
 package edu.kh.oop.practice.model.service;
 
-import java.util.Scanner;
-
 import edu.kh.oop.practice.model.vo.Book;
 
 public class BookService {
 	
-	private Scanner sc = new Scanner(System.in);
 	
-	private Book bookInfo = null;
-	
-	private Book bookInfo2 = null;
 	
 	public void practice() {
-		bookInfo = new Book() {};
+
+		Book book1 = new Book();
 		
-		bookInfo2 = new Book("자바의 정석", 30000, 0.2, "남궁성");
+		Book book2 = new Book("자바의 정석", 30000, 0.2, "남궁성");
 		
-		System.out.println(bookInfo);
+		System.out.println(book1.toString());
 		
-		System.out.println(bookInfo2);
+		System.out.println(book2.toString());
 		
 		System.out.println("===============================");
 		
+		book1.setTitle("C언어");
+		book1.setPrice(30000);
+		book1.setDiscountRate(0.1);
+		book1.setAuthor("홍길동");
+		
 		System.out.println("수정된 결과 확인");
 		
-		bookInfo.setTitle("C언어");
-		bookInfo.setPrice(30000);
-		bookInfo.setDiscountRate(0.1);
-		bookInfo.setAuthor("홍길동");
-		
-		System.out.println(bookInfo);
-		
-		System.out.println("도서명 = " + bookInfo.getTitle());
-		System.out.println("할인된 가격 = " + (bookInfo.getPrice() - (bookInfo.getPrice() * bookInfo.getDiscountRate()) + "원"));
-		System.out.println("도서명 = " + bookInfo2.getTitle());
-		System.out.println("할인된 가격 = " + (bookInfo2.getPrice() - (bookInfo2.getPrice() * bookInfo2.getDiscountRate()) + "원"));
+		System.out.println(book1.toString());
 
+		System.out.println("===============================");
+		System.out.printf("도서명 = %s\n", book1.getTitle());
+		System.out.printf("할인된 가격 = %d원\n", 
+					(int)(book1.getPrice() - (book1.getPrice() * book1.getDiscountRate()))
+				);
+		System.out.printf("도서명 = %s\n", book2.getTitle());
+		System.out.printf("할인된 가격 = %d원\n", 
+				(int)(book2.getPrice() - (book2.getPrice() * book2.getDiscountRate()))
+				);
 		
 		
 	}
